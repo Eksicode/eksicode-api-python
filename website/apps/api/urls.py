8612ad0566d779c6f246561a014a46a94dd4b30c
iamnotagentleman <realveli24@gmail.com>
2019-10-10 15:09:53 +0300

    requirements file name reworked

--- website/apps/api/urls.py
+++ website/apps/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import views as api_views
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 urlpatterns = [
-    path('hello', api_views.HelloView.as_view(), name="hello"),
+    path('hello/', api_views.HelloView.as_view(), name="hello"),
     path('api/token/', obtain_jwt_token, name='token_obtain_pair'),
     path('api/token/refresh/', refresh_jwt_token, name='token_refresh'),
     path('api/token/verify', verify_jwt_token, name='token_verify'),
