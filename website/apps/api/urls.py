0218731a67b3c4e63415d03809f9aeed26bd8f3a
iamnotagentleman <realveli24@gmail.com>
2019-10-10 16:00:37 +0300

    -JWT libary changed | Reason: old JWT libary is outdated.

--- website/apps/api/urls.py
+++ website/apps/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from .views import views as api_views
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
+
     path('hello/', api_views.HelloView.as_view(), name="hello"),
-    path('api/token/', obtain_jwt_token, name='token_obtain_pair'),
-    path('api/token/refresh/', refresh_jwt_token, name='token_refresh'),
-    path('api/token/verify', verify_jwt_token, name='token_verify'),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
 ]
