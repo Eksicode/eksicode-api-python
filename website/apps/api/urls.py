5886101cb0ec5319fe693311887d20f78c2b6674
Işık Kaplan <isik.kaplan@hotmail.com>
2019-10-26 19:19:40 +0300

    Add router and urls to api

--- website/apps/api/urls.py
+++ website/apps/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import views as api_views
-from rest_framework_simplejwt import views as jwt_views
-urlpatterns = [
 
-    path('hello/', api_views.HelloView.as_view(), name="hello"),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
+from . import views
+from .router import RooterWithUrls
+
+router = RooterWithUrls()
+router.register(r'resources', views.ResourceViewSet, basename='resource')
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
