e2800390e8f413714213a54970341d9fa3cefec9
Işık Kaplan <isik.kaplan@hotmail.com>
2019-10-27 15:44:50 +0300

    change wsgi settings from copy-pasted settings to website.settings

--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -13,7 +13,7 @@ from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sourandperky.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
 application = get_wsgi_application()
 
